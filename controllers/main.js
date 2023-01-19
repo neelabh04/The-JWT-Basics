b1834ae8e9d9ff79574242ed01a69397ea24d587
Neelabh Singh <neelabh041997@gmail.com>
2023-01-19 22:35:03 +0530

    Logic for dashboard and Initial authentication middleware setup

--- controllers/main.js
+++ controllers/main.js
@@ -8,26 +8,36 @@ const login = async(req, res) => {
     if(!username || !password) {
         throw new CustomErrorAPI('Please provide email and password', 400)
     }
-
+    
     // just for demo, normally provided by DB!!
     const id = new Date().getDate();
-
+    
     // keep payload small, better user experience
     const token = jwt.sign({id, username}, process.env.JWT_SECRET, {expiresIn: '30d'});
-
+    
     // res.send('Fake login/Register/Signup Route');
     res.status(200).json({msg: 'User created', token});
 }
 
 const dashboard = async(req, res) => {
-    console.log(req.headers);
-    const luckyNumber = Math.floor(Math.random() * 100);
-    res.status(200).json(
-        {
-            msg: 'Hello, John Carter',
-            secretNumber: `Your lucky number is ${luckyNumber}` 
-        }
-    )
+    const authHeader = req.headers.authorization;
+    if(!authHeader || !authHeader.startsWith('Bearer ')) {
+        throw new CustomErrorAPI('No token provided', 401);
+    }
+    
+    const token = authHeader.split(' ')[1];    
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const luckyNumber = Math.floor(Math.random() * 100);
+        res.status(200).json(
+            {
+                msg: `Hello, ${decoded.username}`,
+                secretNumber: `Your lucky number is ${luckyNumber}`,
+            }
+        )
+    } catch (error) {
+        throw new CustomErrorAPI('Not authorized to access this route', 401);
+    }
 }
 
 module.exports = {
